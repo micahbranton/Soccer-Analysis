f3dbb05d87953f0fbc5dc5d59b6dc95d49b5bb87
andrebrener <brener.andre@gmail.com>
2017-01-02 18:21:37 -0300

    update

---
+++ radars/dms_graph_comparison_2.py
@@ -0,0 +1,130 @@
+import seaborn as sns  # improves plot aesthetics
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+table = pd.read_csv('dms2.csv', sep=';', index_col=0)
+
+player_data = {}
+
+for row in table.iterrows():
+    index, data = row
+    temp = []
+    temp.append(data.tolist())
+    player_data[index] = temp[0]
+
+
+def _invert(x, limits):
+    """inverts a value x on a scale from
+    limits[0] to limits[1]"""
+    return limits[1] - (x - limits[0])
+
+
+def _scale_data(data, ranges):
+    """scales data[1:] to ranges[0],
+    inverts if the scale is reversed"""
+    for d, (y1, y2) in zip(data[1:], ranges[1:]):
+        assert (y1 <= d <= y2) or (y2 <= d <= y1)
+    x1, x2 = ranges[0]
+    d = data[0]
+    if x1 > x2:
+        d = _invert(d, (x1, x2))
+        x1, x2 = x2, x1
+    sdata = [d]
+    for d, (y1, y2) in zip(data[1:], ranges[1:]):
+        if y1 > y2:
+            d = _invert(d, (y1, y2))
+            y1, y2 = y2, y1
+        sdata.append((d - y1) / (y2 - y1)
+                     * (x2 - x1) + x1)
+    return sdata
+
+
+class ComplexRadar():
+
+    def __init__(self, fig, variables, ranges,
+                 n_ordinate_levels=6):
+        angles = np.arange(90, 450, 360. / len(variables))
+
+        axes = [fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True,
+                             label="axes{}".format(i))
+                for i in range(len(variables))]
+        l, text = axes[0].set_thetagrids(angles,
+                                         labels=variables, color='1')
+        [txt.set_rotation(angle - 90) for txt, angle
+         in zip(text, angles)]
+        for ax in axes[1:]:
+            ax.patch.set_visible(False)
+            ax.grid("off")
+            ax.xaxis.set_visible(False)
+        for i, ax in enumerate(axes):
+            grid = np.linspace(*ranges[i],
+                               num=n_ordinate_levels)
+            gridlabel = ["{}".format(round(x, 2))
+                         for x in grid]
+            if ranges[i][0] > ranges[i][1]:
+                grid = grid[::-1]  # hack to invert grid
+                # gridlabels aren't reversed
+            gridlabel[0] = ""  # clean up origin
+            gridlabel[1] = ""
+            ax.set_rgrids(grid, labels=gridlabel,
+                          angle=angles[i], fontsize=14, color='0.4')
+            # ax.spines["polar"].set_visible(False)
+            ax.set_ylim(*ranges[i])
+        # variables for plotting
+        self.angle = np.deg2rad(np.r_[angles, angles[0]])
+        self.ranges = ranges
+        self.ax = axes[0]
+
+    def plot(self, data, *args, **kw):
+        sdata = _scale_data(data, self.ranges)
+        self.ax.plot(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+
+    def fill(self, data, *args, **kw):
+        sdata = _scale_data(data, self.ranges)
+        self.ax.fill(self.angle, np.r_[sdata, sdata[0]], *args, **kw)
+
+# example data
+variables = (
+    "Pases Largos",
+    "Pases Correctos %",
+    "Intercepciones",
+    "Quites",
+    "Perdidas",
+    "Duelos Aereos Ganados %",
+    "Fouls")
+
+
+player1_data_raw = player_data['Mussis']
+player1_data = []
+
+for data in player1_data_raw:
+    if data == 0:
+        new_data = 0.001
+    else:
+        new_data = data
+    player1_data.append(new_data)
+
+player2_data_raw = player_data['Mussis 2']
+player2_data = []
+
+for data in player2_data_raw:
+    if data == 0:
+        new_data = 0.001
+    else:
+        new_data = data
+    player2_data.append(new_data)
+
+ranges = [(0.01, 6.5), (30, 90), (0.1, 5.30), (0.1, 5.7),
+          (5.80, 0.39), (0.001, 70.00), (4.9, 0.1)]
+
+# plotting
+fig1 = plt.figure(figsize=(9, 9))
+radar = ComplexRadar(fig1, variables, ranges)
+radar.plot(player2_data, 'dodgerblue')
+radar.fill(player2_data, 'dodgerblue', alpha=0.2)
+radar.plot(player1_data, 'orangered')
+radar.fill(player1_data, 'orangered', alpha=0.2)
+plt.show()
