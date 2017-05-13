13c34b1066e1e317a93c4752e7dc25dfcd1c5d21
andre <brener.andre@gmail.com>
2017-05-12 21:46:27 -0300

    Reorganize repo

--- other_graphs/compare_team_players/team_players_comparison.py
+++ other_graphs/compare_team_players/team_players_comparison.py
@@ -2,51 +2,46 @@
 #          File: graphs.py
 #        Author: Andre Brener
 #       Created: 31 Jan 2017
-# Last Modified: 11 Feb 2017
+# Last Modified: 12 May 2017
 #   Description: description
 # =============================================================================
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('river_data.csv')
+df = pd.read_csv('slo_data.csv')
 
 players_in_graph = 7
 chosen_player = 'Bergessio'
-team = 'River'
+team = 'San Lorenzo'
 
 stats_dict = {
     'total_shots': [
         'es el más tirador del equipo',
         'Top {0} de jugadores de {1} con más tiros por 90 minutos'.format(
-            players_in_graph,
-            team),
-        1],
+            players_in_graph, team), 1
+    ],
     'sot': [
         'fue el que más tiró al arco del equipo',
-        'Top {0} de jugadores  de {1} con más tiros al arco por 90 minutos'.format(
-            players_in_graph,
-            team),
-        1],
+        'Top {0} de jugadores  de {1} con más tiros al arco por 90 minutos'.
+        format(players_in_graph, team), 1
+    ],
     'goals': [
         'fue el más goleador del equipo en el Torneo',
         'Top {0} de jugadores de {1} con más goles por 90 minutos'.format(
-            players_in_graph,
-            team),
-        1],
+            players_in_graph, team), 1
+    ],
     'key_passes': [
         'fue el que hizo más pases que terminaron en tiro',
-        'Top {0} de jugadores de {1} que hicieron más pases que terminaron  en tiro por 90 minutos'.format(
-            players_in_graph,
-            team),
-        1],
+        'Top {0} de jugadores de {1} que hicieron más pases que terminaron  en tiro por 90 minutos'.
+        format(players_in_graph, team), 1
+    ],
     'assists': [
         'fue el mejor asistidor de {0} en el Torneo'.format(team),
-        'Top {0} de jugadores de {1} con más asistencias por 90 minutos'.format(
-            players_in_graph,
-            team),
-        1]}
-
+        'Top {0} de jugadores de {1} con más asistencias por 90 minutos'.
+        format(players_in_graph, team), 1
+    ]
+}
 
 for col in df.columns:
     if col != 'player':
@@ -80,7 +75,7 @@ for col in df.columns:
         ax.set_yticklabels([])
         ax.text(
             0,
-            1.10 * df[col][: players_in_graph].max(),
+            1.10 * df[col][:players_in_graph].max(),
             # 'Tiros totales recibidos',
             '{0}'.format(stats_dict[col][1]),
             color='0.3',
@@ -91,13 +86,12 @@ for col in df.columns:
                 value = '{0}%'.format(int(stats_dict[col][3] * val))
             else:
                 value = round(val, 2)
-            ax.annotate(value,
-                        xy=(x[n],
-                            val),
-                        xytext=(x[n],
-                                1.01 * val),
-                        verticalalignment='bottom',
-                        horizontalalignment='center',
-                        color='0.3',
-                        fontsize=18)
+            ax.annotate(
+                value,
+                xy=(x[n], val),
+                xytext=(x[n], 1.01 * val),
+                verticalalignment='bottom',
+                horizontalalignment='center',
+                color='0.3',
+                fontsize=18)
         plt.show()
